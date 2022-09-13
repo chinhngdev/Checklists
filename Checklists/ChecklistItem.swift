0f3966dc1d0f19e4adc15ab2c0079e119e67d161
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2022-09-13 14:17:34 +0700

    add data model

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Chinh Ng on 9/12/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
