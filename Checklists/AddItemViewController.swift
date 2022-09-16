b139c1f1ad37230e922731cd17e5129f6eb8ad5f
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2022-09-16 11:40:10 +0700

    add navigation controller, add the ability to delete rows, add the Add item screen

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Chinh Ng on 9/16/22.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+}
--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -12,6 +12,7 @@ class ChecklistViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let item1 = ChecklistItem()
         item1.text = "Walk the dog"
@@ -55,12 +56,24 @@ class ChecklistViewController: UITableViewController {
         } else {
             cell.accessoryType = .none
         }
-        
     }
     
     func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
         let label = cell.viewWithTag(1000) as! UILabel
         label.text = item.text
+    }
+    
+    // MARK: - Actions
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        item.text = "I am a new row"
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
     }
 }
 
@@ -79,6 +92,15 @@ extension ChecklistViewController {
         configureCheckmark(for: cell, with: item)
         
         return cell
+    }
+    
+    /// Swipe to delete
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // 1
+        items.remove(at: indexPath.row)
+        // 2
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
     }
 }
 
