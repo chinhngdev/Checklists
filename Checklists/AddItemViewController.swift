eab70cb018c0f60f96949117b844dce9083d5b59
ChinhNg <22429215+chinhngdev@users.noreply.github.com>
2022-09-20 16:57:10 +0700

    add uitextfield, disable cell selection

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+        tableView.allowsSelection = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
     
     @IBAction func cancel() {
@@ -19,6 +27,40 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Table View Delegates
+extension AddItemViewController {
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+}
+
+// MARK: - Text Field Delegates
+extension AddItemViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+//        let oldText = textField.text ?? ""
+//        guard let stringRange = Range(range, in: oldText) else { return false }
+//        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        let oldText = NSString(string: textField.text ?? "")
+        let newText = oldText.replacingCharacters(in: range, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 }
